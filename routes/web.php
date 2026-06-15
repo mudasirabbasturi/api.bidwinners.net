18a720500244125eec3277980a352547b565b51f
Mudasir Abbas Turi <mudasirabbas577@gmail.com>
2026-06-15 23:36:15 +0500

    remove permission

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ use App\Http\Controllers\DirectChatController;
 Route::get('/', function () {
     return view('welcome');
 });
- Route::get('/user-permissions', [ProjectChatController::class, 'getUserPermissions']);
+
  Route::get('/chat-project-list', [ProjectChatController::class, 'chatProjectList'])->name('chat.project.list');
  Route::get('/chat-user-list', [DirectChatController::class, 'chatUserList'])->name('chat.user.list');
